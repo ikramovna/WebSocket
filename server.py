c21fbeb0f57defa1710a38a8fef85e9d87ade1f4
muslima <muslimazokirjonova2004@gmail.com>
2023-11-23 22:25:49 +0500

    Initial commit

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import psycopg2
 
+
 def initialize_database():
     conn = psycopg2.connect(
         host="localhost",
@@ -16,7 +17,9 @@ def initialize_database():
     conn.commit()
     conn.close()
 
+
 connected_users = []
+
 
 def handle_private_chat(user1, user2):
     while True:
@@ -42,30 +45,28 @@ def handle_private_chat(user1, user2):
     connected_users.remove(user1)
     connected_users.remove(user2)
 
+
 def handle_client(client_socket, client_address):
     print(f"Accepted connection from {client_address}")
 
-    # Get the username from the client
     client_socket.send("Enter your username: ".encode('utf-8'))
     username = client_socket.recv(1024).decode('utf-8').strip()
 
-    # Add the user to the database
     cursor = db_conn.cursor()
-    cursor.execute("INSERT INTO users (username, in_chat) VALUES (%s, %s) ON CONFLICT (username) DO NOTHING;", (username, False))
+    cursor.execute("INSERT INTO users (username, in_chat) VALUES (%s, %s) ON CONFLICT (username) DO NOTHING;",
+                   (username, False))
     db_conn.commit()
 
     connected_users.append({'socket': client_socket, 'username': username})
 
     print(f"{username} connected.")
 
-    # Pair users if possible
     if len(connected_users) >= 2:
         user1 = connected_users.pop(0)
         user2 = connected_users.pop(0)
 
         print(f"Pairing {user1['username']} with {user2['username']}.")
 
-        # Notify users about the private chat
         user1['socket'].send(f"You are now in a private chat with {user2['username']}".encode('utf-8'))
         user2['socket'].send(f"You are now in a private chat with {user1['username']}".encode('utf-8'))
 
@@ -74,6 +75,7 @@ def handle_client(client_socket, client_address):
 
     else:
         client_socket.send("Waiting for a partner. Please be patient.".encode('utf-8'))
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 5555))
@@ -95,6 +97,3 @@ while True:
 
     client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
     client_handler.start()
-
-
-
